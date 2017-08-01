6af992835846bb717034587d48f9194e1a10ab96
Tarik Benammar <tarik@benammar.com>
2017-08-01 18:09:06 +0200

    make both LE and regular certificate optional

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,44 +1,30 @@
 #!/bin/bash
 
-set -euo pipefail
-
 # Validate environment variables
 
 MISSING=""
 
-[ -z "${DOMAIN}" ] && MISSING="${MISSING} DOMAIN"
+if [ "${LE_DOMAIN}" == "" ]; then
+  if  [  "${SSL_DOMAIN}" == "" ]; then
+    MISSING="MISSING LE_DOMAIN or SSL_DOMAIN variable"
+  fi 
+fi
+
 [ -z "${UPSTREAM}" ] && MISSING="${MISSING} UPSTREAM"
 
-
-if [ "${MISSING}" != "" ]; then
-  echo "Missing required environment variables:" >&2
-  echo " ${MISSING}" >&2
-  exit 1 
-  fi
-
-#Processing DOMAIN into an array
-DOMAINSARRAY=($(echo "${DOMAIN}" | awk -F ";" '{for(i=1;i<=NF;i++) print $i;}'))
-echo "Provided domains"
-printf "%s\n" "${DOMAINSARRAY[@]}"
-  
-#Processing UPSTREAM into an array
-UPSTREAMARRAY=($(echo "${UPSTREAM}" | awk -F ";" '{for(i=1;i<=NF;i++) print $i;}'))
-echo "Services to reverse-proxy"
-printf "%s\n" "${UPSTREAMARRAY[@]}"
-
-#The two arrays should have the same lenght
-if [ "${#DOMAINSARRAY[@]}" != "${#UPSTREAMARRAY[@]}" ]; then
-  echo "The number of domains must match the number of upstream services"
-fi
+set -eo pipefail
 
 # Default other parameters
 SERVER=""
 [ -n "${STAGING:-}" ] && SERVER="--server https://acme-staging.api.letsencrypt.org/directory"
 
-DOMAINALIASARRAY=""
-[ -n "${DOMAINALIAS:-}" ] && DOMAINALIASARRAY=($(echo "${DOMAINALIAS}" | awk -F ";" '{for(i=1;i<=NF;i++) print $i;}')) && echo "Provided domains alias" && printf "%s\n" "${DOMAINALIASARRAY[@]}"
+[ -n "${LE_DOMAIN:-}" ] && [ -z "${EMAIL}" ] && MISSING="${MISSING} EMAIL"
 
-[ -n "${DOMAINALIAS:-}" ] && [ -z "${EMAIL}" ] && MISSING="${MISSING} EMAIL"
+if [ "${MISSING}" != "" ]; then
+  echo "Missing required environment variables:" >&2
+  echo " ${MISSING}" >&2
+  exit 1
+fi
 
 # Generate strong DH parameters for nginx, if they don't already exist.
 if [ ! -f /etc/ssl/dhparams.pem ]; then
@@ -68,101 +54,94 @@ mkdir -p /etc/nginx/vhosts/
   echo "Rendering template of nginx.conf"
   sed -e "s/\${DOMAIN}/${DOMAIN}/g" \
       -e "s/\${UPSTREAM}/${UPSTREAM}/" \
-      /templates/nginx.conf > "$dest"
+/templates/nginx.conf > "$dest"
 
+if [ "${SSL_DOMAIN}" != "" ]; then
 
-# Process templates
-upstreamId=0
-letscmd=""
-for t in "${DOMAINSARRAY[@]}"
-do
-  dest="/etc/nginx/vhosts/$(basename "${t}").conf"
-  src="/templates/vhost.sample.conf"
+ # Process templates
+ letscmd=""
+ dest="/etc/nginx/vhosts/$(basename "${SSL_DOMAIN}").conf"
+ src="/templates/vhost.sample.conf"
 
-  if [ -r /configs/"${t}".conf ]; then
-    echo "Manual configuration found for $t"
-    src="/configs/${t}.conf"
-  fi
+ if [ -r /configs/"${SSL_DOMAIN}".conf ]; then
+   echo "Manual configuration found for $t"
+   src="/configs/${SSL_DOMAIN}.conf"
+ fi
 
-  echo "Rendering template $src of $t in $dest"
-  sed -e "s/\${DOMAIN}/${t}/g" \
-      -e "s/\${UPSTREAM}/${UPSTREAMARRAY[upstreamId]}/" \
-      -e "s/\${PATH}/${DOMAINSARRAY[0]}/" \
-      "$src" > "$dest"
+ echo "Rendering template $src in $dest"
+ sed -e "s/\${DOMAIN}/${SSL_DOMAIN}/g" \
+  	    -e "s/\${UPSTREAM}/${UPSTREAM}/" \
+ 	    -e "s/\${PATH}/${SSL_DOMAIN}/" \
+	    "$src" > "$dest"
 
-  upstreamId=$((upstreamId+1))
-done
-
-if [ ! -n "${DOMAINALIAS:-}" ]; then
-  echo Ready
-  # Launch nginx in the foreground
-  /usr/sbin/nginx -g "daemon off;"
-  exit 
+ if [ ! -n "${SSL_DOMAIN:-}" ]; then
+   echo Ready
+   # Launch nginx in the foreground
+   /usr/sbin/nginx -g "daemon off;"
+   exit 
+ fi
 fi
+# Process LE SSL template
 
-# Process ALIAS templates
-upstreamId=0
-letscmd=""
-for t in "${DOMAINALIASARRAY[@]}"
-do
-  dest="/etc/nginx/vhosts/$(basename "${t}").conf"
+if [ "${LE_DOMAIN}" != "" ]; then
+
+  letscmd=""
+  dest="/etc/nginx/vhosts/$(basename "${LE_DOMAIN}").conf"
   src="/templates/vhost.le-ssl.sample.conf"
 
-  if [ -r /configs/"${t}".conf ]; then
+  if [ -r /configs/"${LE_DOMAIN}".conf ]; then
     echo "Manual configuration found for $t"
-    src="/configs/${t}.conf"
+    src="/configs/${LE_DOMAIN}.conf"
   fi
 
-  echo "Rendering template $src of $t in $dest"
-  sed -e "s/\${DOMAIN}/${t}/g" \
-      -e "s/\${UPSTREAM}/${UPSTREAMARRAY[upstreamId]}/" \
-      -e "s/\${PATH}/${DOMAINALIASARRAY[0]}/" \
+  echo "Rendering template $src of $LE_DOMAIN in $dest"
+  sed -e "s/\${DOMAIN}/${LE_DOMAIN}/g" \
+      -e "s/\${UPSTREAM}/${UPSTREAM}/" \
+      -e "s/\${PATH}/${LE_DOMAIN}/" \
       "$src" > "$dest"
 
-  upstreamId=$((upstreamId+1))
 
-  #prepare the letsencrypt command arguments
-  letscmd="$letscmd -d $t "
-done
+#prepare the letsencrypt command arguments
+  letscmd="$letscmd -d $LE_DOMAIN "
 
 # Check if the SAN list has changed
-if [ ! -f /etc/letsencrypt/san_list ]; then
- cat <<EOF >/etc/letsencrypt/san_list
- "${DOMAINALIAS}"
+ if [ ! -f /etc/letsencrypt/san_list ]; then
+  cat <<EOF >/etc/letsencrypt/san_list
+  "${SSL_DOMAIN}"
 EOF
-  fresh=true
-else
-  old_san=$(cat /etc/letsencrypt/san_list)
-  if [ "${DOMAINALIAS}" != "${old_san}" ]; then
-    fresh=true
-  else
-    fresh=false
-  fi
-fi
+   fresh=true
+ else
+   old_san=$(cat /etc/letsencrypt/san_list)
+   if [ "${SSL_DOMAIN}" != "${old_san}" ]; then
+     fresh=true
+   else
+     fresh=false
+   fi
+ fi
 
 # Initial certificate request, but skip if cached
-  if [ $fresh = true ]; then
-    echo "The SAN list has changed, removing the old certificate and ask for a new one."
-    rm -rf /etc/letsencrypt/{live,archive,keys,renewal}
+   if [ $fresh = true ]; then
+     echo "The SAN list has changed, removing the old certificate and ask for a new one."
+     rm -rf /etc/letsencrypt/{live,archive,keys,renewal}
 
-   echo "certbot certonly "${letscmd}" \
-    --standalone --text \
-    "${SERVER}" \
-    --email "${EMAIL}" --agree-tos \
-    --expand " > /etc/nginx/lets
+    echo "certbot certonly "${letscmd}" \
+     --standalone --text \
+     "${SERVER}" \
+     --email "${EMAIL}" --agree-tos \
+     --expand " > /etc/nginx/lets
 
-    echo "Running initial certificate request... "
-    /bin/bash /etc/nginx/lets
-  fi
+     echo "Running initial certificate request... "
+     /bin/bash /etc/nginx/lets
+   fi
 
 #update the stored SAN list
-echo "${DOMAINALIAS}" > /etc/letsencrypt/san_list
+ echo "${LE_DOMAIN}" > /etc/letsencrypt/san_list
 
 #Create the renewal directory (containing well-known challenges)
-mkdir -p /etc/letsencrypt/webrootauth/
+ mkdir -p /etc/letsencrypt/webrootauth/
 
 # Template a cronjob to reissue the certificate with the webroot authenticator
-echo "Creating a cron job to keep the certificate updated"
+ echo "Creating a cron job to keep the certificate updated"
   cat <<EOF >/etc/periodic/monthly/reissue
 #!/bin/sh
 
@@ -181,11 +160,13 @@ ${SERVER} \
 /usr/sbin/nginx -s reload
 EOF
 
-chmod +x /etc/periodic/monthly/reissue
+ chmod +x /etc/periodic/monthly/reissue
 
 # Kick off cron to reissue certificates as required
 # Background the process and log to stderr
-/usr/sbin/crond -f -d 8 &
+ /usr/sbin/crond -f -d 8 &
+
+fi #LE_DOMAIN Section
 
 echo Ready
 # Launch nginx in the foreground
